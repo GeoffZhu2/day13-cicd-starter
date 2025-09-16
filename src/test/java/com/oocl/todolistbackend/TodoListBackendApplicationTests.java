ee4fde09d79874149a3ec77a03cfc299e7ba6b74
Geoff Zhu <zhuge2@oocl.com>
2025-09-16 10:56:26 +0800

    init springBoot

---
+++ src/test/java/com/oocl/todolistbackend/TodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.todolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
